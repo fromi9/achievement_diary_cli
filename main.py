88540ce167321ed42bc9c1d74f01de1c52964411
Stephan <goodfree16@gmail.com>
2025-04-10 13:47:38 +0900

    Added the ability to delete records

--- main.py
+++ main.py
@@ -36,6 +36,58 @@ def add_achievement():
 
     save_entry(date, text)
     print(Fore.GREEN + "Saved")
+
+def search_achievements():
+    print(Style.BRIGHT + "\n*** SEARCH ***")
+    keyword = input("Enter a word to search: ").strip()
+
+    if keyword:
+        entries = read_entries()
+        matches = [entry for entry in entries if keyword.lower() in entry.lower()]
+        
+        if matches:
+            print("\n--- Search Results ---\n")
+            for match in matches:
+                print(match)
+                print("-" * 30)
+        else:
+            print(Fore.YELLOW + "No entries found.")
+            print()
+    else:
+        print(Fore.RED + "Empty input. Try again")
+
+def delete_entries():
+    entries = read_entries()
+
+    if not entries:
+        print(Fore.YELLOW + "No entries to delete.")
+        return
+    
+    for idx, entry in enumerate(entries):
+        print(f"{idx}:")
+        print(entry)
+        print("-" * 30)
+
+    try:
+        entry_numbers = input("Enter the numbers of the entries to delete, separated by commas: ").strip()
+        entry_numbers = [int(num.strip()) for num in entry_numbers.split(",")]
+    except ValueError:
+        print(Fore.RED + "Invalid input. Must be numbers separated by commas.")
+        return
+    
+    valid_entries = [entry for idx, entry in enumerate(entries) if idx not in entry_numbers]
+    removed_entries = [entries[idx] for idx in entry_numbers if 0 <= idx < len(entries)]
+
+    if removed_entries:
+        with open(file_ach, "w", encoding="utf-8") as file:
+            file.write("\n\n".join(valid_entries) + "\n")
+        print()
+        print(Fore.RED + "Entries DELETED:")
+        for removed in removed_entries:
+            print(Fore.CYAN + removed)
+    else:
+        print(Fore.RED + "Invalid entry numbers.")
+
     
 def view_achievements():
     entries = read_entries()
@@ -63,7 +115,9 @@ def main():
         print(Style.BRIGHT + "*** ACHIEVEMENT DIARY ***")
         print("1 - Add achievement")
         print("2 - View achievements")
-        print("3 - Exit")
+        print("3 - Search achievements")
+        print("4 - Delete entry")
+        print("5 - Exit")
 
         choice = input(Style.BRIGHT + "Enter choice: ")
 
@@ -74,6 +128,12 @@ def main():
             view_achievements()
 
         elif choice == "3":
+            search_achievements()
+        
+        elif choice == "4":
+            delete_entries()
+
+        elif choice == "5":
             print("BYE")
             break
         else:
